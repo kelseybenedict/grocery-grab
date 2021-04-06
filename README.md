fc84842fd189e98bb3f251b85d9c30811d62f3bb
Lasette <lasette.brown@gmail.com>
2021-04-05 21:34:30 -0600

    psuedocode files added.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # grocery-grab
+
+## Front End
+
+- Figure out handlebars
+- Make html and css pages like normal
+- convert them into handlebars format
+- front end javascript
+- log in and log out
+- make it pretty
+
+## Back End
+
+- create server
+- create routes (get post put delete)
+- create api database
+- Authentication and sessions
+- script testing
+
+### pseudocode
---
+++ frontend.js
@@ -0,0 +1,35 @@
+//Front End Pseudocode:
+
+//1 - PAGE ONE when someone loads our ap they see the landing page 
+    //- When they click on the login button 
+        //2 - PAGE TWO - a page loads asking them to log in
+
+    //- When they click on 'create New Account'
+        //3 - PAGE THREE - a page loads that allows them to create an account
+
+//- AFter login
+  //4 - PAGE FOUR - they are redirected to the list page
+
+  //- any lists they have previously made are displayed in a box to the left.
+
+  //- clicking on any of the list titles on the left displays the content of that list to the right
+
+  //- when they click on 'add item' - a drop down appears that allows them to type an item and save it to the selected list.
+
+    //- when the delete icon is clicked - the coresponding item is deleted from the list.
+
+ //- when the checked icon is clicked - the item is crossed off with a single strike through
+    //when the 'checked' box is clicked
+    //mark item as isChecked=true
+    //change css applied to that item
+
+//- when it is unclicked the item no longer has a strike through
+
+  //- when they click on the 'create new grocery list' button
+    //- a dropdown appears asking the user what they would like their new list to be called.
+        //- when they submit the new list name
+        //- this new list appears at the bottom of the left hand box
+        // - clicking the new list will show an empty list on the right hand side. 
+
+
+

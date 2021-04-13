8711be02903cb9633413dcc2812de5c1bff70b77
Lasette <lasette.brown@gmail.com>
2021-04-12 21:19:25 -0600

    trying out adding a put

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -15,6 +15,26 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+// router.put('/:id', withAuth, async (req, res) => {
+//   try {
+//     const projectData = await Project.update({
+//       where: {
+//         id: req.params.id,
+//         user_id: req.session.user_id,
+//       },
+//     });
+
+//     if (!projectData) {
+//       res.status(404).json({ message: 'No project found with this id!' });
+//       return;
+//     }
+
+//     res.status(200).json(projectData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const projectData = await Project.destroy({
